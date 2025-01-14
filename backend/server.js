78c129b72d847037a76c0bccb3039b1726f7e185
TerenzTuason <terenzjetcemtuason@gmail.com>
2025-01-14 22:14:02 +0800

    feat: add POST method to backend

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 const db = mysql.createConnection({
   host: process.env.HOST,
@@ -17,12 +19,47 @@ app.get("/", (req, res) => {
   return res.json("From Backend Side");
 });
 
+// GET all users
 app.get("/users", (req, res) => {
   const sql = "SELECT * FROM USERS";
   db.query(sql, (err, data) => {
-    if(err) return res.json(err);
+    if (err) return res.json(err);
     return res.json(data);
-  })
+  });
+});
+
+// POST route to add a user
+app.post("/users", (req, res) => {
+  const { 
+    username, 
+    first_name, 
+    middle_name, 
+    last_name, 
+    email_address, 
+    password, 
+    phone_number, 
+    user_type 
+  } = req.body;
+
+  const sql = `INSERT INTO users 
+    (username, first_name, middle_name, last_name, email_address, password, phone_number, user_type) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+
+  const values = [
+    username,
+    first_name,
+    middle_name,
+    last_name,
+    email_address,
+    password,
+    phone_number,
+    user_type,
+  ];
+
+  db.query(sql, values, (err, result) => {
+    if (err) return res.status(500).json(err);
+    return res.status(201).json({ message: "User added successfully", userId: result.id });
+  });
 });
 
 app.listen(process.env.PORT, () => {
